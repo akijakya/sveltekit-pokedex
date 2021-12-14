d54c25cf6588c199155cf7bb4a334f28a0c30072
Andras Jaky <jakyandras@gmail.com>
2021-12-14 17:20:39 +0100

    feat: refactored getting pokemon from store to preload

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,47 @@
+<!-- this block will run before the page content gets rendered out -> server-side rendering -->
+<script context="module">
+    export async function load({page}) {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=150`
+        const res = await fetch(url)
+        const data = await res.json()
+        const loadedPokemon = data.results.map((data, index) => {
+            return {
+                name: data.name,
+                id: index + 1,
+                image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                    index + 1
+                }.png`
+            }
+        })
+        return {props: {pokemon: loadedPokemon}}
+    }
+</script>
+
 <script>
-	import {pokemon} from "../stores/pokestore"
-	import PokemanCard from "../components/pokemanCard.svelte"
+    import PokemanCard from "../components/pokemanCard.svelte"
+    export let pokemon
+    let searchTerm = ""
+    let filteredPokemon = []
 
-	let searchTerm = ""
-	let filteredPokemon = []
-
-	$: {
-		if (searchTerm) {
-			filteredPokemon = $pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()))
-		} else {
-			filteredPokemon = [...$pokemon] // creating a copy of the original data with the spread operator
-		}
-	}
+    $: {
+        if (searchTerm) {
+            filteredPokemon = pokemon.filter(pokeman => pokeman.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        } else {
+            filteredPokemon = [...pokemon] // creating a copy of the original data with the spread operator
+        }
+    }
 </script>
 
 <svelte:head>
-	<!-- This is the title in the browser tab -->
-	<title>SvelteKit Pokedex</title>
+    <!-- This is the title in the browser tab -->
+    <title>SvelteKit Pokedex</title>
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">SvelteKit Pokedex</h1>
 
 <input class="w-full rounded-md text-lg p-4 border-2" type="text" bind:value={searchTerm} placeholder="Search Pokemon">
 
 <div class="py-4 grid gap-4 md:grid-cols-2 grid-cols-1">
-	{#each filteredPokemon as pokeman}
-		<PokemanCard pokeman={pokeman}/>
-	{/each}
+    {#each filteredPokemon as pokeman}
+        <PokemanCard pokeman={pokeman}/>
+    {/each}
 </div>
